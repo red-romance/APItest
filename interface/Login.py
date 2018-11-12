d62b107dfe4514cfba047de8d5c5e4ae80ff5680
cherry <626655108@qq.com>
2018-11-12 18:11:09 +0800

    parameterized

--- interface/Login.py
+++ interface/Login.py
@@ -8,30 +8,13 @@ class Login(object):
     login data convert
     '''
     
-    def login_phone(self, phone_num, phone_id):
-        lo = pull.LoginParam()
-        lo.account = phone_num
-        lo.login_type = pull.LoginParam.PHONE
-        #SerializeToString
-        lo_data = lo.SerializeToString()
-        
-        po = pull.PullServiceRequest()
-        po.app_id = phone_id
-        po.action_type = pull.PullServiceRequest.LOGIN
-        po.param = lo_data
-        
-        po_data = po.SerializeToString()
+    def login_phone(self, name, pwd):
+
         
         return po_data
     
     
     def login_analyse(self, response):
-        
-        result = pull.PullServiceResponse()
 
-        result.ParseFromString(response.content)
-    
-        login_res = pull.LoginResult()
-        login_res.ParseFromString(result.result)
         
-        return login_res.login_status
+        return login_res
--- testcase/LoginTest.py
+++ testcase/LoginTest.py
@@ -6,29 +6,25 @@ import os
 from interface.Login import Login
 from common.request import request
 from common.ParseCSV import ParseCSV
+from Parameterized import parameterized
 
 
 class Test(unittest.TestCase):
+	
+	@parameterized.expend([
+	("aa", "qwe", "登录成功")
+	])
+    def test_01(self, name, pwd, assert_text):
 
-    
-    def test_01(self):
+		data = Login.login_phone(self, name, pwd)
+		response = request.pull_post(self, data)
 
-        login_data = ParseCSV(os.getcwd() +'/data/login.csv').readCSV()
-        print(login_data)
-
-        for i in range(len(login_data)):
-
-            data = 0
-            if login_data[i][1].lower() == "phone":
-                data = Login.login_phone(self, login_data[i][2], login_data[i][3])
-            response = request.pull_post(self, login_data[i], data)
-
-            #analyze the response for add assert
-            req_result = Login.login_analyse(self, response)
-            
-            #add assert
-            n = int(login_data[i][4])
-            self.assertEqual(n, req_result)
+		#analyze the response for add assert
+		req_result = Login.login_analyse(self, response)
+		
+		#add assert
+		n = int(assert_text)
+		self.assertEqual(n, req_result)
             
 
 if __name__ == "__main__":
